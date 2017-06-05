473e89aff65e5ed8cfb680e829967359d744f71b
EmmAlva <clioemma_28@hotmail.com>
2017-06-05 12:50:38 -0500

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+'use strict' //permite escribir ECMA6 válido
+
+const render = (root)=>{
+	root.empty()
+};
+
+$(document).ready(function (){
+	alert("lhasd");
+}); 
+
+
+
+$( _ =>{
+
+	const root = $('.root');
+	render(root);
+});
